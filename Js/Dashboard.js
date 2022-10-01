a7376529e90c9c5ffe0fa36aaa77bd18176354c1
Stephen Muchara <68949651+smuchara@users.noreply.github.com>
2022-10-01 16:07:10 +0300

    Changing Z-Index for the sub-menu-wrap

--- Js/Dashboard.js
+++ Js/Dashboard.js
@@ -5,15 +5,20 @@ let hamburgermenuBtn = document.querySelector("#nav-icon1");
 let myaccountBtn = document.querySelector("#myaccount");
 let submenuwrap = document.querySelector(".sub-menu-wrap");
 let dropdown = document.querySelector(".bx.bx-chevron-down");
+let rating = document.querySelector(".rating");
+
 searchBtn.onclick = function () {
     sidebar.classList.toggle("active")
     hamburgermenuBtn.classList.toggle("open")
+    rating.classList.toggle("active")
 }
 hamburgermenuBtn.onclick = function () {
     sidebar.classList.toggle("active")
     hamburgermenuBtn.classList.toggle("open")
+    rating.classList.toggle("active")
+
 }
 myaccountBtn.onclick = function () {
     submenuwrap.classList.toggle("active")
-    dropdown.classList.toggle("active")
+    rating.classList.toggle("open")
 }
