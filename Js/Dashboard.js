845aa48d6036c4e7ea91da8baf55bf8ff10f844d
Stephen Muchara <68949651+smuchara@users.noreply.github.com>
2022-10-03 16:14:14 +0300

    Creating Darkmode theme

--- Js/Dashboard.js
+++ Js/Dashboard.js
@@ -6,6 +6,11 @@ let myaccountBtn = document.querySelector("#myaccount");
 let submenuwrap = document.querySelector(".sub-menu-wrap");
 let dropdown = document.querySelector(".bx.bx-chevron-down");
 let rating = document.querySelector(".rating");
+let toggleDarkMode = document.querySelector("#darkmode");
+let homecontent = document.querySelector(".home_content");
+let body = document.querySelector(".body");
+let cardcontainer = document.querySelectorAll(".card-container");
+
 
 searchBtn.onclick = function () {
     sidebar.classList.toggle("active")
@@ -21,4 +26,14 @@ hamburgermenuBtn.onclick = function () {
 myaccountBtn.onclick = function () {
     submenuwrap.classList.toggle("active")
     rating.classList.toggle("open")
+}
+
+toggleDarkMode.onclick = function () {
+    body.classList.toggle("drkmode");
+    sidebar.classList.toggle("drkmode")
+    hamburgermenuBtn.classList.toggle("drkmode")
+    homecontent.classList.toggle("drkmode")
+    for (var i = 0; i < cardcontainer.length; i++) {
+        cardcontainer[i].classList.toggle('drkmode');
+    }
 }
