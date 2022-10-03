d2309fdae2c939285968255b9db83ecd2cb297f7
Stephen Muchara <68949651+smuchara@users.noreply.github.com>
2022-10-03 23:09:23 +0300

    Adding a chat and notification drop-down widget

--- Js/Dashboard.js
+++ Js/Dashboard.js
@@ -10,7 +10,12 @@ let toggleDarkMode = document.querySelector("#darkmode");
 let homecontent = document.querySelector(".home_content");
 let body = document.querySelector(".body");
 let cardcontainer = document.querySelectorAll(".card-container");
-
+let messageButton = document.querySelector(".messageButton")
+let notificationButton = document.querySelector(".notificationButton")
+let notificationMenu = document.querySelector(".notificationMenu")
+let notificationMenu2 = document.querySelector(".notificationMenu2")
+let lightmodetxt = document.querySelector(".txt2")
+let darkmodetxt = document.querySelector(".txt")
 
 searchBtn.onclick = function () {
     sidebar.classList.toggle("active")
@@ -21,14 +26,20 @@ hamburgermenuBtn.onclick = function () {
     sidebar.classList.toggle("active")
     hamburgermenuBtn.classList.toggle("open")
     rating.classList.toggle("active")
-
+    submenuwrap.classList.remove("active")
+    notificationMenu.classList.remove("active")
+    notificationMenu2.classList.remove("active")
 }
 myaccountBtn.onclick = function () {
     submenuwrap.classList.toggle("active")
-    rating.classList.toggle("open")
+    notificationMenu.classList.remove("active")
+    notificationMenu2.classList.remove("active")
+    submenuwrap.classList.replace("close", "active")
 }
 
 toggleDarkMode.onclick = function () {
+    lightmodetxt.classList.toggle("active")
+    darkmodetxt.classList.toggle("active")
     body.classList.toggle("drkmode");
     sidebar.classList.toggle("drkmode")
     hamburgermenuBtn.classList.toggle("drkmode")
@@ -36,4 +47,21 @@ toggleDarkMode.onclick = function () {
     for (var i = 0; i < cardcontainer.length; i++) {
         cardcontainer[i].classList.toggle('drkmode');
     }
+    notificationMenu.classList.toggle("drkmode")
+    notificationMenu2.classList.toggle("drkmode")
+
 }
+messageButton.onclick = function () {
+    notificationMenu.classList.toggle("active")
+    submenuwrap.classList.toggle("close")
+    notificationMenu2.classList.replace("active", "close")
+    submenuwrap.classList.remove("active")
+}
+
+notificationButton.onclick = function () {
+    notificationMenu2.classList.toggle("active")
+    submenuwrap.classList.toggle("close")
+    notificationMenu.classList.replace("active", "close")
+    notificationMenu2.classList.replace("close", "active")
+}
+
